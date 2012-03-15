9d9c8d1b0e809625c20112fd0af084d45da12523
Benj Fredrick <benj@dtek.net>
2012-03-15 13:21:29 -0600

    css and js for the email intake form.

--- sites/all/themes/occupy/template.php
+++ sites/all/themes/occupy/template.php
@@ -10,3 +10,14 @@
  * for your subtheme grows. Please read the README.txt in the /preprocess and /process subfolders
  * for more information on this topic.
  */
+
+/**
+ * Implements hook_preprocess_page().
+ */
+function occupy_preprocess_html(&$vars) {
+  // add our in-field css/js
+  drupal_add_css('sites/all/themes/occupy/css/ows_infield_labels.css');
+  drupal_add_js('sites/all/libraries/jquery.infieldlabel/jquery.infieldlabel.min.js');
+  drupal_add_js('sites/all/themes/occupy/js/ows_infield_labels.js');
+  
+}
